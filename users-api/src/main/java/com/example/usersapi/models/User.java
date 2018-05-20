71014832b56b499aebf435792e6dc3067dcb4ea7
Ted Anyansi <tanyansi@gmail.com>
2018-05-20 00:37:27 -0700

    added model, repository for User entity. also added flyway migration files to add columns to the user table and an afterMigration file to add some rows

---
+++ users-api/src/main/java/com/example/usersapi/models/User.java
@@ -0,0 +1,30 @@
+package com.example.usersapi.models;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+@Entity @Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+}
---
+++ users-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS
+	(USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD)
+VALUES
+	('Taylor Swift', 'Taylor', 'Swift', 'tdog@gmail.com', 'tdizzy'),
+	('Gucci Mane', 'Gucci', 'Mane', 'gucci@gmail.com', 'guccigetem'),
+	('Mariah Carey', 'Mariah', 'Carey', 'eve@gmail.com', 'mariahcarey' ),
+	('Jidenna', 'Jidenna', 'Stanford', 'jidenna@gmail.com', 'jidennaMan');
