395cc4fdc9a901129a270bc68e79c7d81fcbeead
Ted Anyansi <tanyansi@gmail.com>
2018-05-21 11:14:11 -0700

    completed add user and delete user methods from front-end

--- front-end/src/app/users/users.component.ts
+++ front-end/src/app/users/users.component.ts
@@ -8,21 +8,66 @@ import { Http } from '@angular/http';
 })
 export class UsersComponent implements OnInit {
   users = [
-    {
-      firstName: 'Gucci'
-    }
   ];
-  constructor(private http: Http) { }
+
+  newUser: any;
+  constructor(private http: Http) {
+    this.newUser = {
+
+    };
+  }
   ngOnInit() {
     this.fetchUsers();
   }
   fetchUsers() {
-    this.http.get('/users')
+    this.http.get('/api/users')
       .toPromise()
       .then( (response) => {
         this.users = response.json();
+        console.log('ALL USERS', this.users);
       });
+  }
+  addUser() {
+    this.http.post('/api/users', this.newUser)
+    .toPromise()
+    .then( (response) => {
+      console.log(response);
+      console.log(response.json());
+      this.users.push(response.json());
+      console.log(this.users);
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+  }
 
+  editUser(id, thisBinding) {
+    console.log('ID', id);
+    console.log('THIS BINDING', thisBinding);
+  }
+
+  deleteUser(id, ...args) {
+    console.log('ID', id);
+    let index;
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].id === id) {
+        index = i;
+      }
+    }
+    console.log('INDEX TO DELETE', index);
+    console.log('INDEXOF USERS TO DELETE', this.users.indexOf(args[0]));
+    console.log('THIS BINDING', args);
+    this.http.delete('/api/users/' + id)
+      .toPromise()
+      .then( (response) => {
+        console.log(this.users);
+        this.users.splice(index, 1);
+        console.log('RESPONSE', response);
+        console.log(this.users);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
   }
 
 }
